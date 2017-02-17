5177361f59bc19a02ecc30b5ad5bfa7ab82a2007
mtachot <matthieu.tachot@hotmail.fr>
2017-02-17 15:26:27 +0100

    Ajout Templates, Databinding, Converter

---
+++ WpfApplication1/WpfApplication1/Classes/Converters/AgeToBrushConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApplication1.Classes.Converters
+{
+    public class AgeToBrushConverter : IValueConverter
+    {
+        private const int SEUIL_AGE = 18;
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // Si l'âge est inférieur au seuil défini, il sera coloré en rouge, sinon en vert
+            SolidColorBrush redBrush = new SolidColorBrush(Color.FromArgb(255, 160, 0, 0));
+            SolidColorBrush greenBrush = new SolidColorBrush(Color.FromArgb(255, 0, 120, 0));
+            if (!String.IsNullOrEmpty(value.ToString()) && int.Parse(value.ToString()) < SEUIL_AGE)
+                return redBrush;
+            else
+                return greenBrush;
+        }
+
+        // Ne sera jamais utilisé
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfApplication1/WpfApplication1/Classes/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApplication1.Classes
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        // Convertir les données pour l'IHM
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (value is bool && (bool)value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        // Convertir les données de l'IHM vers l'objet métier
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value is Visibility && (Visibility)value == Visibility.Visible;
+        }
+    }
+}
