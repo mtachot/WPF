22cafc2544b7586395649fd3b95ae88cd191caae
mtachot <matthieu.tachot@hotmail.fr>
2017-02-27 10:34:02 +0100

    Add bindablebase

---
+++ BindableBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.Classes
+{
+    public abstract class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public bool SetProperty<T>(T value, ref T storage, [CallerMemberName]string propName = null)
+        {
+            if (!Object.Equals(storage, value))
+            {
+                storage = value;
+                OnPropertyChanged(propName);
+                return true;
+            }
+            return false;
+        }
+    }
+}
